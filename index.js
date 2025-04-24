afa8c81c4542f85aff50add08347794486ea2cc8
nikeedev <nikita.gon.dev@gmail.com>
2025-04-24 22:07:45 +0200

    up

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ const port = 8081;
 app.use(bodyParser.json());
 app.use(cors()); 
 
-const users_db = require('better-sqlite3')('users.db', options);
-const users = db.prepare('SELECT * FROM users WHERE id = ?').get(userId);
+const db = require('better-sqlite3')('messago.db');
+db.pragma('journal_mode = WAL');
+const users = db.prepare('SELECT * FROM users;').all();
+const userinfo = db.prepare('SELECT * FROM userinfo;').all();
+const posts = db.prepare('SELECT * FROM posts;').all();
+
+// console.log(users.find(e => e.username == username));
 
 const tokens = {
 
@@ -23,12 +28,21 @@ app.get('/', (req, res) => {
 
 app.post('/token', (req, res) => {
     const { username, password } = req.body;
-    if (users[username] == undefined || users[username] == null) {
+    
+    let user = users.find(e => {
+        console.log("e:",e.username);
+
+        return e.username == username;
+    });
+
+    console.log("username: '%s'", username);
+
+    if (user.username == undefined || user.username == null) {
         res.statusCode = 403;
         res.send(JSON.stringify({ status: "error", message: `Username ${username} doesn't exist in the db. Are you registered?`}));
     }
 
-    let hash = users[username].password_hash;
+    let hash = user.password_hash;
     
     if (bcrypt.compareSync(password, hash)) {
         if (Object.hasOwn(username)) {
@@ -66,5 +80,6 @@ app.post('/check', (req, res) => {
 app.listen(port, () => {
     console.log(`messago api server starting at http://localhost:${port}`);
 });
+
 
 
